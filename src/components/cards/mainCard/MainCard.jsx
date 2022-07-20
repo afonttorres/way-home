5ade967710813cf92b366717a0e2024b529cf2fe
Agnès Font Torres <afonttorres@gmail.com>
2022-07-20 22:46:59 +0200

    routing added to MainCard to Detail

--- src/components/cards/mainCard/MainCard.jsx
+++ src/components/cards/mainCard/MainCard.jsx
@@ -1,7 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import '../mainCard/mainCard.css';
 export const MainCard = ({ housing }) => {
+
+    const navigate = useNavigate();
+
     return (
-        <article className = 'cardContainer'>
+        <article onClick={() => navigate(`/detail/${housing.id}`)} className='cardContainer'>
             <div className='cardImage'>
                 <img src={housing.imgUrl1} alt="img"></img>
                 {/* <div className='star'>★{housing.star}</div> */}
@@ -9,7 +13,7 @@ export const MainCard = ({ housing }) => {
             <div className='cardText'>
                 <span className='tag'>{housing.category}</span>
                 <span className='title'><h3>{housing.title}</h3></span>
-                <span className='location'><img className ='icon' src="https://img.icons8.com/fluency-systems-regular/48/000000/marker--v1.png"/>{housing.location}</span>
+                <span className='location'><img className='icon' src="https://img.icons8.com/fluency-systems-regular/48/000000/marker--v1.png" />{housing.location}</span>
                 <span className='price'>{housing.price}€/per month</span>
             </div>
         </article>
