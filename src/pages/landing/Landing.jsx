ec29982fb3c11203516b78d4e57ad226beab348b
laiaafernande18 <laiaafernandez19@gmail.com>
2022-07-21 13:05:53 +0200

    Landing desing

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,4 +1,6 @@
 import { LandingButton } from "../../components/buttons/LandingButton"
+import houseG from "../../assets/houseG.avif";
+import "../landing/landing.css";
 
 export const Landing = () => {
 
@@ -7,8 +9,17 @@ export const Landing = () => {
         <div className="landing-wrapper col">
             <LandingButton />
             {/* boton para ir a about us */}
+        
+             <div className="houseG">
+                 <img src={houseG} alt="houseG" />
+            </div>
+
+            <h2 className="tittle-lang">Find your Perfect Place</h2>
+            <p>you and your pets</p>
         </div>
         
+
+
     )
 }
 
